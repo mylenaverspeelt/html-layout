5f9c0dccb67a2503b5bdecc768020830d7b71777
mylenaverspeelt <mylena49@hotmail.com>
2024-06-18 15:29:44 -0300

    improve addTaskDay function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@ function sendUsername() {
     const username = usernameInput.value
 
     const inputedUsername = document.getElementById('inputed-username')
-    const newUser = inputedUsername.innerHTML = username
+    const newUser = inputedUsername.innerText = username
 
     const okButton = document.getElementById('ok-button')
 
@@ -20,15 +20,26 @@ function addDayTask() {
 
     const inputNewTask = document.getElementById('new-day-task')
 
+    if (inputNewTask.value === '') {
+        alert('Por favor, informe qual a tarefa');
+        return;
+    }
+
+    const taskExists = dayTasks.some(task => task.name === inputNewTask.value.trim());
+
+    if (taskExists) {
+        alert('Esta tarefa já existe');
+        return;
+    }
+
     const newTask = {
-        name: inputNewTask.value,
+        name: inputNewTask.value.trim(),
         id: dayTasks.length + 1
     }
 
     dayTasks.push(newTask)
 
     inputNewTask.value = '';
-
 
     showDayTasks()
 
@@ -38,6 +49,7 @@ function addDayTask() {
 function showDayTasks() {
 
     const ordenedList = document.getElementById('day-list');
+    ordenedList.innerHTML = '';
 
     return dayTasks.forEach(task => {
         const newListItem = document.createElement('li');
